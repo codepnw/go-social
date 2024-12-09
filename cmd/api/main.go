b48b7e315e9daafe20f62298c6578894d625cd9a
codepnw <idchokms01@outlook.com>
2024-12-09 20:34:42 +0700

    implement repository pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 
 	"github.com/codepnw/social/internal/env"
+	"github.com/codepnw/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
- 
+
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 
 	mux := app.mount()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/codepnw/social
 
 go 1.23.0
 
-require github.com/go-chi/chi/v5 v5.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/lib/pq v1.10.9
+)

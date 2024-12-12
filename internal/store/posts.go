d16e4b0e173672b80548117e5ea44fc957d63b92
codepnw <idchokms01@outlook.com>
2024-12-12 12:22:20 +0700

    feat/posts: crud post handler

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,18 +3,20 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/lib/pq"
 )
 
 type Post struct {
-	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
-	Title     string   `json:"title"`
-	UserID    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	CreatedAt string   `json:"created_at"`
-	UpdatedAt string   `json:"updated_at"`
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+	Comments  []Comment `json:"comments"`
 }
 
 type PostStore struct {
@@ -40,6 +42,67 @@ func (s *PostStore) Create(ctx context.Context, p *Post) error {
 	)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
+	query := `
+		SELECT id, user_id, title, content, tags, created_at, updated_at
+		FROM posts
+		WHERE id = $1;
+	`
+
+	var post Post
+
+	err := s.db.QueryRowContext(ctx, query, id).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		pq.Array(&post.Tags),
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &post, nil
+}
+
+func (s *PostStore) Update(ctx context.Context, post *Post) error {
+	query := `UPDATE posts SET title = $1, content = $2 WHERE id = $3;`
+
+	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *PostStore) Delete(ctx context.Context, postID int64) error {
+	query := `DELETE FROM posts WHERE id = $1;`
+
+	res, err := s.db.ExecContext(ctx, query, postID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrNotFound
 	}
 
 	return nil
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,20 +3,32 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+)
+
+var (
+	ErrNotFound = errors.New("resource not found")
 )
 
 type Storage struct {
 	Posts interface {
+		GetByID(context.Context, int64) (*Post, error)
 		Create(context.Context, *Post) error
+		Update(context.Context, *Post) error
+		Delete(context.Context, int64) error
 	}
 	Users interface {
 		Create(context.Context, *User) error
+	}
+	Comments interface {
+		GetByPostID(context.Context, int64) ([]Comment, error)
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostStore{db: db},
-		Users: &UserStore{db: db},
+		Posts:    &PostStore{db: db},
+		Users:    &UserStore{db: db},
+		Comments: &CommentStore{db: db},
 	}
 }

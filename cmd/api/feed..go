d4715aca342b9e6c068e609f228062b1ad4ec48d
codepnw <idchokms01@outlook.com>
2024-12-18 19:50:40 +0700

    feat/users: user feed pagination and sorting

--- cmd/api/feed..go
+++ cmd/api/feed..go
@@ -1,11 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/codepnw/social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.errorBadRequest(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.errorBadRequest(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(155))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(155), fq)
 	if err != nil {
 		app.errorInternalServer(w, r, err)
 		return
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -20,7 +20,7 @@ type Storage struct {
 		Create(context.Context, *Post) error
 		Update(context.Context, *Post) error
 		Delete(context.Context, int64) error
-		GetUserFeed(context.Context, int64) ([]PostWithMetadata, error)
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
 	}
 	Users interface {
 		GetByID(context.Context, int64) (*User, error)

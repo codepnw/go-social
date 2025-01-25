a31358319c286da537b1fa3ac95fe9f0a77ca44d
codepnw <idchokms01@outlook.com>
2025-01-25 19:40:22 +0700

    server: server metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"expvar"
+	"runtime"
 	"time"
 
 	"github.com/codepnw/social/internal/auth"
@@ -129,6 +131,15 @@ func main() {
 		authenticator: jwtAuthenticator,
 		rateLimiter:   rateLimiter,
 	}
+
+	// Metrics collected
+	expvar.NewString("version").Set(version)
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
 
 	mux := app.mount()
 
